d1219a4c464879d7f0fd11c134fd69ba9139f8c3
Mr. FaHaD <155773006+RJ-FaHaD@users.noreply.github.com>
2024-05-04 04:09:27 +0500

    CountDown

--- Clock/app.js
+++ Clock/app.js
@@ -1,39 +1,39 @@
-var Hours = document.getElementById('Hours')
-var Minutes = document.getElementById('Minutes')
-var Seconds = document.getElementById('Seconds')
-var eid = document.getElementById('eid')
+var Hours = document.getElementById('Hours');
+var Minutes = document.getElementById('Minutes');
+var Seconds = document.getElementById('Seconds');
 
 setInterval(function(){
     var currentDate = new Date();
     
-    Hours.innerText = (currentDate.getHours()<10?"0":"") + currentDate.getHours();
-    Minutes.innerText = (currentDate.getMinutes()<10?"0":"") + currentDate.getMinutes();
-    Seconds.innerText = (currentDate.getSeconds()<10?"0":"") + currentDate.getSeconds();
+    Hours.innerText = (currentDate.getHours() < 10 ? "0" : "") + currentDate.getHours();
+    Minutes.innerText = (currentDate.getMinutes() < 10 ? "0" : "") + currentDate.getMinutes();
+    Seconds.innerText = (currentDate.getSeconds() < 10 ? "0" : "") + currentDate.getSeconds();
 
-//if(Hours.innerText >=12){
-  //  amPm.innerText = Pm
+    // Calculate time until the specified date (06/17/2024)
+    var now = new Date();
+    var eids = new Date("06/17/2024");
+    var when = now.getTime
+    var milliseconds = eids - now;
+    var seconds = Math.floor(milliseconds / 1000);
+    var minutes = Math.floor(seconds / 60);
+    var hours = Math.floor(minutes / 60);
+    var days = Math.floor(hours / 24);
+    var months = Math.floor(days / 30);
+    var years = Math.floor(months / 12);
 
-//else( amPm.innerText = Am
-//Date.getTime()
-// Jan 1 1970 se abhe tk jitne milleseconds guzar gae wo lekar aajata he..
+    console.log("milliseconds=>", milliseconds);
+    console.log("seconds=>", seconds);
+    console.log("minutes=>", minutes);
+    console.log("hours=>", hours);
+    console.log("days=>", days);
+    console.log("months=>", months);
+    console.log("years=>", years);
 
- var now = new Date();
- var eids = new Date("06/17/2024");  //mm/dd/yyyy var milleseconds = now.getTime();
- var eidsMillseconds = eids.getTime();
- var eids = milleseconds - eidsMillseconds;
- console.log("milleseconds=>", eids);
- console.log("seconds=>", Math.round(eids / 1000));
- console.log("minutes=>", Math.round(eids / 1000 / 60));
-console.log("hours=>", Math.round(eids / 1000 / 60 / 60));
-console.log("day=>", Math.round(eids / 1000 / 60 / 60 / 24));
- console.log("months=>", Math.round(eids / 1000 / 60 / 60 / 24 / 30));
- console.log("years=>", Math.round(eids / 1000 / 60 / 60 / 24 / 30 / 12));
+    var eid = document.getElementById('eid').innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
 
-
-
-
-},1000);
-
-
-
-
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EID UL ADHA Is Arrived !!";
+  }
+}, 1000);
